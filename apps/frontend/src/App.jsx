74491b414c15b01fa089e1ea7593af46201afed1
Anish Pillai <anishp8914@gmail.com>
2023-09-11 18:53:11 +0530

    Authentication and User Data storage complete

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,101 +1,83 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css';
 
-// function App() {
-//   const [count, setCount] = useState(0)
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+} from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+import  Dashboard  from "./pages/dashboard/dashboard";
 
-// export default App
+import LandingPage from "./pages/landing/landingpage";
+ 
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+ 
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+ 
 
-import "./App.css";
-import { useState } from "react";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-function App() {
-  const [current, setCurrent] = useState(0);
-  const switchToSignUp = () =>{
-    let option1 = document.querySelector(".login-option");
-    let option2 = document.querySelector(".sign-up-option");
-    if (option2.classList.contains("current")) {
-      return;
-    }
-    let loginForm = document.querySelector(".login-form");
-    loginForm.style.marginLeft = "-1000px";
-    option1.classList.toggle("current");
-    option2.classList.toggle("current");
-    setTimeout(switchSignUp, 230);
-  }
-  function switchToLogIn() {
-    let option1 = document.querySelector(".login-option");
-    let option2 = document.querySelector(".sign-up-option");
-    if (option1.classList.contains("current")) {
-      return;
-    }
-    let loginForm = document.querySelector(".sign-up-form");
-    loginForm.style.marginRight = "-1000px";
-    option1.classList.toggle("current");
-    option2.classList.toggle("current");
-    setTimeout(switchLogIn, 230);
-  }
-  const switchSignUp = () => {
-    setCurrent(1);
-  }
-  function switchLogIn() {
-    setCurrent(0);
-  }
+ 
+
+ 
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+ 
   return (
-    <div className="App">
-      <div className="container">
-        <div className="login-sign-up-option">
-          <div
-            onClick={() => switchToLogIn()}
-            className="login-option option current"
-          >
-            Log In
-          </div>
-          <div
-            onClick={() => switchToSignUp()}
-            className="sign-up-option option"
-          >
-            Sign Up
-          </div>
-        </div>
-        {current === 0 ? (
-          <Login current={current} setCurrent={setCurrent} switchToSignUp={switchToSignUp} switchSignUp={switchSignUp} />
-        ) : (
-          <Signup current={current} setCurrent={setCurrent} switchToLogIn={switchToLogIn} switchLogIn={switchLogIn} />
-        )}
-      </div>
-    </div>
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        
+          <Route
+            path="/sign-in/*"
+            element={
+              <div className="authentication">
+                <SignIn redirectUrl="/protected" routing="path" path="/sign-in" />
+              </div>
+            }
+          />
+        
+          <Route
+            path="/sign-up/*"
+            element={
+            <div className="authentication">
+            <SignUp redirectUrl="/protected" routing="path" path="/sign-up" />
+            </div>}
+          />
+        
+        
+        <Route
+          path="/protected"
+          element={
+          <>
+            <SignedIn >
+              <Dashboard />
+            </SignedIn>
+             <SignedOut>
+              <LandingPage />
+           </SignedOut>
+          </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
   );
 }
-
+ 
+function App() {
+  return (
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
+  );
+}
+ 
 export default App;
-
---
+++ apps/frontend/src/pages/landing/landingpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+
+        <h1>Homies4U landingpage</h1>
+        <a href="/sign-in">Sign In</a>
+        <a href="/sign-up">Sign Up</a>
+        <h2><a href="/protected">Go to Dashboard page</a></h2>
+
+    </div>
+  )
+}
+
+export default LandingPage;
